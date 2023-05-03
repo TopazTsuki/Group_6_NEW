0861dfcf4b8881fd8a3e7efed81fb02935e11bb2
salorta7 <salvador.orta@marquette.edu>
2023-05-03 14:13:41 -0500

    customer class

---
+++ bin/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+{
+
+    namespace FinalProject
+{
+    public class Customer
+    {
+        private static int autoIncreament;
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Customer()
+        {
+            autoIncreament++;
+            Id = autoIncreament;
+        }
+    }
+}

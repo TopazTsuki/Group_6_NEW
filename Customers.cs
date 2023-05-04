a8be244a26ab6e5e01f05a0fa98862c40791d06b
Chris <cferacota7@gmail.com>
2023-05-03 21:42:47 -0500

    Draft 1  Order method.

--- Customers.cs
+++ Customers.cs
@@ -1,8 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace ClassProject.Model
 {
-	public Class1()
-	{
-	}
+    public class Customers
+    {
+        public List<Customer> customers { get; set; }
+
+        public Customers()
+        {
+            customers = new List<Customer>();
+        }
+
+        public Customer Authenticate(string username, string password)
+        {
+            var c = customers.Where(o => (o.Username == username) && (o.Password == password));
+
+            if (c.Count() > 0)
+            {
+                return c.First();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
 }
